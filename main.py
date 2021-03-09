059a8414548350530b171c3de2280242595cac2b
ChengTseLu <72141509+ChengTseLu@users.noreply.github.com>
2021-03-09 10:31:31 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -9,8 +9,12 @@ if __name__ == "__main__":
     data.setup(True)
     # data.data_filtering()
     # data.data_augment()
-    data.data_addNoise(0, 2)
+    # data.data_addNoise(0, 2)
     dataset = data.get_dataset()
+    
+    # show dataset in channels (visualize data preprocessing)
+    # figure = Plot(dataset=dataset)
+    # figure.show_dataset()
 
     models = Model(dataset[0].shape[1:], 50)
     m = models.cnn2()
@@ -19,6 +23,8 @@ if __name__ == "__main__":
     train = Train(m, dataset, 50, 200, 'adam', 'sparse_categorical_crossentropy')
     train.training()
     train.evaluate()
+
+    
 
 
 # tensorboard --logdir logs/scalars --port=7000
--- plot.py
+++ plot.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 class Plot:
-    def __init__(self, loss_hist, dataset=None):
-        self.hist = loss_hist.history
+    def __init__(self, loss_hist=None, dataset=None):
+        if loss_hist != None:
+            self.hist = loss_hist.history
         self.dataset = dataset
 
     def draw(self):
@@ -23,5 +25,18 @@ class Plot:
 
         plt.show()
 
-    def test(self):
+    def show_dataset(self):
         plt.figure()
+
+        avg = np.mean(self.dataset[0], axis=0)
+        name = []
+        i = 0
+        for channel in avg:
+            plt.plot(channel)
+            i += 1
+            name.append('channel: ' + str(i))
+            
+        plt.legend(name)
+        plt.show()
+
+        
